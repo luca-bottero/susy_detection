b88c30e2d9115e8c12bfb9052dbe98e5a22e488e
luca-bottero <lucabottero99@gmail.com>
2021-01-16 12:33:46 +0100

    Update .gitignore, Datas_corr_matrix.png, and 2 more files...

---
+++ main.py
@@ -0,0 +1,27 @@
+#%%
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+#%%
+datas = pd.read_csv('./Data/SUSY.csv')
+datas.columns = ["label","lepton 1 pT"," lepton 1 eta"," lepton 1 phi"," lepton 2 pT"," lepton 2 eta"," lepton 2 phi"," missing energy magnitude"," missing energy phi"," MET_rel"," axial MET"," M_R"," M_TR_2"," R"," MT2"," S_R"," M_Delta_R"," dPhi_r_b"," cos(theta_r1)"]
+
+#%%
+#Exploratory analisys
+datas.head()
+
+datasHist = datas.hist(bins = 50, figsize = (20,20))
+plt.savefig("Datas_histograms.png", facecolor = 'white')
+
+#%%
+plt.figure(figsize = (20,20))
+datasCorr = plt.imshow(datas.corr(), cmap='viridis', interpolation='none')
+plt.colorbar(datasCorr)
+plt.savefig("Datas_corr_matrix.png", facecolor = "white")
+
+
+
+# %%
+
+# %%
