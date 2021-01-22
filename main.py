1f72792616dbfaa9714990f5ada621a6b1a87730
luca-bottero <lucabottero99@gmail.com>
2021-01-22 17:30:43 +0100

    Update NN_model.png and main.py

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error, accuracy_score, roc_auc_score
 
-
 #%%
 datas = pd.read_csv('./Data/SUSY.csv')
 datas.columns = ["label","lepton 1 pT"," lepton 1 eta"," lepton 1 phi"," lepton 2 pT"," lepton 2 eta"," lepton 2 phi"," missing energy magnitude"," missing energy phi"," MET_rel"," axial MET"," M_R"," M_TR_2"," R"," MT2"," S_R"," M_Delta_R"," dPhi_r_b"," cos(theta_r1)"]
@@ -28,7 +27,6 @@ plt.figure(figsize = (20,20))
 datasCorr = plt.imshow(datas.corr(), cmap='viridis', interpolation='none')  #Datas correlation matrix (default: Fischer)
 plt.colorbar(datasCorr)
 plt.savefig("./Results/Datas_corr_matrix.png", facecolor = "white")
-
 
 #%%
 #Data preparation
@@ -55,20 +53,18 @@ XGB = xgb.train(XGBParam, dtrain, num_round)
 
 end = time.time()
 
-XGBScore = np.sqrt(mean_squared_error(XGB.predict(xgb.DMatrix(X_test)),y_test))
-XGBAccuracy = accuracy_score(XGB.predict(xgb.DMatrix(X_test)).round(),y_test)
-XGBAUC = roc_auc_score(XGB.predict(xgb.DMatrix(X_test)).round(),y_test)
+XGBScore = mean_squared_error(XGB.predict(xgb.DMatrix(X_test)),y_test)          #Mean squared error
+XGBAccuracy = accuracy_score(XGB.predict(xgb.DMatrix(X_test)).round(),y_test)   #Accuracy
+XGBAUC = roc_auc_score(XGB.predict(xgb.DMatrix(X_test)).round(),y_test)         #AUC score
 print("Elapsed time: " + str(end - start))
 #print("Root Mean Square error: " + str(XGBScore))
 print("Fraction of correctly labeled data: " + str(XGBAccuracy))
 print("Area under ROC curve: " + str(XGBAUC))
 
-
 # %%
 #NEURAL NETWORK MODEL
 
 InputNum = len(datas.columns) - 1
-
 
 inputs = keras.Input(shape=(InputNum,), name="Input")
 
@@ -83,11 +79,12 @@ model = keras.Model(inputs=[inputs], outputs=[outputs])
 
 model.summary()
 
-simple_sgd = keras.optimizers.Adadelta(lr=0.02)  
+simple_sgd = keras.optimizers.Adadelta(lr = 0.01)  
 model.compile(loss='binary_crossentropy', optimizer=simple_sgd, metrics=['accuracy'])
  
 max_epochs = 25
 h = model.fit(X_train, y_train, batch_size=32, epochs=max_epochs, verbose = 2)
+
 #%%
 keras.utils.plot_model(model, "NN_model.png", show_shapes=True)
 
@@ -102,15 +99,35 @@ plt.ylabel('log(loss)')
 plt.xlabel('epoch')
 # %%
 predictions = np.round(model.predict(X_test))
+indices = [i for i in enumerate(predictions) if predictions[i] != y_test[i]]
+#subset_of_wrongly_predicted = [X_test[i] for i in indices ]
 
 print(predictions)
 print(y_test)
-print(predictions != y_test)
+#print(subset_of_wrongly_predicted)
 
-incorrects = X_test[predictions != y_test]
-incorrects
+# %%
+CorrectIdx = []   #list of correctly labeled datas
+WrongIdx = []     #list of wrongly labeled datas
+
+a = time.time()
+
+for i in range(100):
+  index = X_test.iloc[[i]].index.values
+  item = X_test.iloc[[i]].to_numpy()
+  prediction = np.round(model.predict(item))
+  truth = y_test.iloc[[i]].to_numpy()
+
+  if prediction == truth:
+    CorrectIdx.append(index)
+  else:
+    WrongIdx.append(index)
+
+print(time.time() - a)
+
 
 # %%
 
-
+#%%
+print(len(CorrectIdx))
 # %%
